5e5911ab4bdf72cfc5859b9d35125eb1f2f0bc3f
micah-jaffe <micah.jaffe95@gmail.com>
2018-11-13 14:33:02 -0800

    Create edit form and #update in CatsController class

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -8,4 +8,37 @@ class CatsController < ApplicationController
     @cat = Cat.find(params[:id])
     render :show
   end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cats_url
+    else
+      render json: @cat.errors.full_messages
+    end
+  end
+
+  def new
+    render :new
+  end
+
+  def update
+    cat = Cat.find(params[:id])
+
+    if cat.update(cat_params)
+      redirect_to cat_url(cat)
+    else
+      render json: cat.errors.full_messages
+    end
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+    render :edit
+  end
+
+  private
+  def cat_params
+    params.require(:cat).permit(:name, :color, :sex, :description, :birth_date)
+  end
 end
